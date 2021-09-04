00ec9f0de1a50c3cad93bb01dc0f89c86d7ff350
Asifakhan10 <89301467+Asifakhan10@users.noreply.github.com>
2021-09-04 19:05:30 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function clr() {
+    document.getElementById("result").value=" "
+}
+function  solve(){
+    let x= document.getElementById("result").value
+    let y= eval(x)
+    document.getElementById("result").value=y
+}
+function dis(val)
+{
+    document.getElementById("result").value+=val
+}
